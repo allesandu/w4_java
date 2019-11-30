789ccbeba46c71489e98edab8d865f97b41e888b
allesandu <allesandu@gmail.com>
2019-11-30 16:15:31 +0200

    Second attemp commit

---
+++ Autopen.java
@@ -0,0 +1,26 @@
+public class Autopen extends Pen {
+    private boolean isOpen;
+    
+    public Autopen() {
+        super(capacity);
+        this.isOpen = false;
+    }
+    
+    public void click() {
+        this.isOpen = !this.isOpen;
+    }
+    
+    public boolean open() {
+        return this.isOpen;
+    }
+    
+    @Override
+    public void write(Paper paper, String content) {
+        throws WritingUtencilException
+    {
+        if ( !this.open() ) {
+            throw new ClosedPenException()
+        }
+        super.write(paper, content);
+    }
+}
